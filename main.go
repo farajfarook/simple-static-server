3dfc18226e1e93c947a87151ddff8934c7dcc0ef
Faraj Farook <farajfarook@gmail.com>
2019-02-26 11:14:58 +1100

    Adding ext and specific file serves

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 	"os"
+
+	"github.com/labstack/echo"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -25,15 +26,20 @@ func serverCmd() *cobra.Command {
 		Run: func(cmd *cobra.Command, args []string) {
 			addr := cmd.Flags().Lookup("addr").Value.String()
 			dir := cmd.Flags().Lookup("dir").Value.String()
-			fs := http.FileServer(http.Dir(dir))
-			http.Handle("/", fs)
+			ext := cmd.Flags().Lookup("ext").Value.String()
+			e := echo.New()
+			e.GET("/:file", func(c echo.Context) error {
+				return c.File(dir + "/" + c.Param("file") + "." + ext)
+			})
 			log.Println("Listening on " + addr)
-			http.ListenAndServe(addr, nil)
+			e.Start(addr)
 		},
 	}
 	cmd.Flags().StringP("dir", "r", ".", "Data Directory")
+	cmd.Flags().StringP("ext", "e", "json", "File extension to serve")
 	cmd.Flags().StringP("addr", "a", ":8080", "API listening address")
 	viper.BindPFlag("dir", cmd.Flags().Lookup("dir"))
+	viper.BindPFlag("ext", cmd.Flags().Lookup("ext"))
 	viper.BindPFlag("addr", cmd.Flags().Lookup("addr"))
 	return cmd
 }
